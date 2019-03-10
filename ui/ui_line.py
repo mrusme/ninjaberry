ad2125c14331b81bb0e1c3f99d642e1c83793206
Marius <marius@twostairs.com>
2019-03-10 17:57:13 +0100

    Implemented UILine

---
+++ ui/ui_line.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding=utf8
+
+from ui.ui_element import UIElement
+from PIL import ImageDraw
+import math
+
+class UILine(UIElement):
+    # draw.line([0, 15, 128, 15], fill=255, width=1, joint=None)
+    def __init__(self, resources = {}, position = [0, 0, 0, 0], size = 1, fill = 255):
+        super().__init__(event_handlers = [self.event], can_focus = False, can_highlight = False)
+        self._resources = resources
+        self._position = position
+        self._size = size
+        self._fill = fill
+
+    def event(self, event, next):
+        print('LINE EVENT')
+        print(event)
+
+        return True
+
+    @property
+    def fill(self):
+        return self._fill
+
+    @fill.setter
+    def fill(self, value):
+        self._fill = value
+
+    def render(self, screen):
+        draw = ImageDraw.Draw(screen)
+        draw.line(self._position, fill=self._fill, width=self._size, joint=None)
+
+        return screen
