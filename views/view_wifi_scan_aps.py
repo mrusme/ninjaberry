1f374a05d7a3f151131644763cdbb1cf02130e66
Marius <marius@twostairs.com>
2019-03-11 17:23:27 +0100

    Implemented timeout, enhanced AP list clearing

--- views/view_wifi_scan_aps.py
+++ views/view_wifi_scan_aps.py
@@ -18,7 +18,6 @@ from ui.ui_list import UIList
 
 from partials.partial_menu import PartialMenu
 
-from bettercap import Bettercap
 from helpers.system import getAvailableIfaces
 
 class ViewWifiScanAps(View):
@@ -41,11 +40,49 @@ class ViewWifiScanAps(View):
             }
         ]
 
+        self._timeout = '5'
+
         self._bettercap = None
         self._thread_scan_aps = None
         self._scanned_aps = []
 
     def callback(self, screen, event = None):
+        self._scanned_aps_to_list()
+        return True
+
+    def event(self, element_id, event, next, payload={}):
+        print('WifiScanAps Event:')
+        print(event)
+        print(payload)
+        self._partial_menu.event(element_id=element_id, event=event, next=next, payload=payload)
+        if event == 'display':
+            self._bettercap = payload['args']['bettercap']
+            # TODO: Handle errors with bettercap
+
+            self._timeout = payload['args']['timeout']
+
+            self._thread_scan_aps = threading.Thread(target=self._thread_bettercap_scan_aps, args=(self._timeout,))
+            self._thread_scan_aps.daemon = True
+            self._thread_scan_aps.start()
+        elif event == 'picked':
+            ssid = payload['id']
+            ap = payload['args']
+            return self._event_handler(element_id=element_id, event='navigate', next=None, payload={ 'to': 'wifi_ap_actions', 'args': { 'bettercap': self._bettercap, 'ap': ap } })
+        elif event == 'conceal':
+            if 'to' in payload:
+                if payload['to'] != 'wifi_ap_actions':
+                    print('Destroying WifiScanAps ..')
+                    self._thread_scan_aps.join()
+                    self._clear_scanned_aps_and_list()
+                    print('Destroyed')
+        return True
+
+    def _thread_bettercap_scan_aps(self, timeout):
+        if self._bettercap != None:
+            self._scanned_aps = self._bettercap.scan_aps(timeout=timeout)
+        return self._scanned_aps
+
+    def _scanned_aps_to_list(self):
         if len(self._scanned_aps) > 0 and len(self._view[1]['element'].entries) != len(self._scanned_aps):
             print('Setting aps')
             ap_list = []
@@ -58,31 +95,7 @@ class ViewWifiScanAps(View):
             self._view[1]['element'].entries = ap_list
         return True
 
-    def event(self, element_id, event, next, payload={}):
-        print('WifiScanAps Event:')
-        print(event)
-        print(payload)
-        self._partial_menu.event(element_id=element_id, event=event, next=next, payload=payload)
-        if event == 'display':
-            self._bettercap = payload['args']['bettercap']
-            # TODO: Handle errors with bettercap
-            self._thread_scan_aps = threading.Thread(target=self.thread_bettercap_scan_aps, args=())
-            self._thread_scan_aps.daemon = True
-            self._thread_scan_aps.start()
-        elif event == 'picked':
-            ssid = payload['id']
-            ap = payload['args']
-            return self._event_handler(element_id=element_id, event='navigate', next=None, payload={ 'to': 'wifi_ap_actions', 'args': { 'bettercap': self._bettercap, 'ap': ap } })
-        elif event == 'conceal':
-            if 'to' in payload:
-                if payload['to'] != 'wifi_ap_actions':
-                    print('Destroying WifiScanAps ..')
-                    self._thread_scan_aps.join()
-                    self._scanned_aps = []
-                    print('Destroyed')
+    def _clear_scanned_aps_and_list(self):
+        self._scanned_aps = []
+        self._view[1]['element'].entries = []
         return True
-
-    def thread_bettercap_scan_aps(self):
-        if self._bettercap != None:
-            self._scanned_aps = self._bettercap.scan_aps()
-        return self._scanned_aps
