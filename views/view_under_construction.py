d8e8cd38147f514960c5692ed50110b8747d4fa2
Marius <marius@twostairs.com>
2019-03-11 12:49:40 +0100

    Implemented UIUnderConstruction

---
+++ views/view_under_construction.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# coding=utf8
+
+import time
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+from views.view import View
+
+from ui.ui_animation import UIAnimation
+from ui.ui_button import UIButton
+from ui.ui_label import UILabel
+from ui.ui_line import UILine
+from ui.ui_list import UIList
+
+class ViewUnderConstruction(View):
+    def __init__(self, resources, event_handler):
+        self._resources = resources
+        self._event_handler = event_handler
+        self._view = [
+            {
+                'id': 'animation_under_construction',
+                'element': UIAnimation(
+                    resources = {},
+                    event_handler = (lambda ev, nxt: self._event_handler('animation_under_construction', ev, nxt)),
+                    position = [0, 0],
+                    size = [(self._resources['display']['width'] - 1), (self._resources['display']['height'] - 1)],
+                    frame_files = ['assets/greatwave.bmp']
+                )
+            },
+            {
+                'id': 'label_title',
+                'element': UILabel(
+                    resources = { 'font': self._resources['fonts']['kosugi'] },
+                    position = [53, 0],
+                    size = [(self._resources['display']['width'] - 1), self._resources['fonts']['kosugi']['size']],
+                    label = '工事中'
+                )
+            }
+        ]
+
+        self._rounds = 0
+
+    def callback(self, screen, event = None):
+        return True
+
+    def event(self, element_id, event, next, payload={}):
+        print('UnderConstruction event')
+        print(event)
+        if event == 'clicked':
+            return self._event_handler(element_id=element_id, event='navigate', next=None, payload={ 'to': 'wifi' })
+        return True
